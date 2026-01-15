3fc941f13cf11d92c89e5c4aa9f928cf704f4704
Ismi Nur Fadilah <isminurfadilah9@gmail.com>
2026-01-15 12:30:00 +0700

    Person B - SERVICE & DISCOUNT: ProductService + DiscountStrategy implementation + unit tests

---
+++ src/main/java/com/upb/agripos/model/ProductPersonB.java
@@ -0,0 +1,75 @@
+package com.upb.agripos.model;
+
+/**
+ * Domain Model untuk Product
+ * Merepresentasikan produk yang dijual di toko
+ * Person B - Person Tugasku
+ */
+public class Product {
+    private String productId;
+    private String name;
+    private double price;
+    private int stock;
+    private String category;
+
+    // Constructor
+    public Product(String productId, String name, double price, int stock, String category) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.category = category;
+    }
+
+    // Getters & Setters
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId='" + productId + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", stock=" + stock +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
